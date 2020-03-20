3e5743f310ca2c3ba420ef6293efc88048406b31
Konrad <konrad.kubiec@gmail.com>
2020-03-20 00:15:03 +0000

    Running the game

--- src/js/logic/gameEngine.js
+++ src/js/logic/gameEngine.js
@@ -34,6 +34,53 @@ export default class GameEngine {
 					this.movesInUse[moveName].concat(playerName) : [playerName]
 			}
         }
+        return moveName;
+	}
+
+	/**
+	 * Execute game to find a winner
+	 */
+	execute() {
+		let leaders = null,
+			winningMove = null;
+
+		const {
+			moves,
+			movesInUse
+		} = this;
+
+		const numberOfMovesInUse = Object.keys(movesInUse).length;
+		const numberOfKnownMoves = Object.keys(moves).length;
+        let loosingMoves = [];
+
+		if (numberOfKnownMoves === numberOfMovesInUse ||
+			numberOfKnownMoves === 1) return TIE;
+
+		for (const move in movesInUse) {
+			const arrayOfPlayers = movesInUse[move];
+
+			if (leaders !== null) {
+				// @ts-ignore
+				if (moves[move].winsWith.indexOf(winningMove) > -1) {
+					loosingMoves.push(winningMove);
+					leaders = arrayOfPlayers;
+                    winningMove = move;
+				}
+			} else {
+				leaders = arrayOfPlayers;
+                winningMove = move;
+                // @ts-ignore
+                loosingMoves.push(...moves[move].winsWith);
+			}
+        }
+        
+        // Closed graph exception
+		if (winningMove !== null &&
+			loosingMoves.indexOf(winningMove) > -1) return TIE;
+
+        // 2+ players use the same winning move
+		if (leaders.length > 1) return TIE;
+		return leaders[0] || "Error!";
 	}
 
 }
