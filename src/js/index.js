8f5db37c5268a30fd430a701d4488d62ca0bba87
Konrad <konrad.kubiec@gmail.com>
2020-03-20 00:18:42 +0000

    running game inside index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,46 @@ import "regenerator-runtime/runtime";
 import logger from './logger';
 
 import '../css/index.scss';
+import GameEngine from '../js/logic/gameEngine';
 
 logger('it works well!');
+
+/**
+ * Based on inputs generate game and output text message with the result
+ * @param {object} inputs 
+ */
+function playRockPaperScissors(inputs) {
+	let output = '';
+
+	const {
+		name,
+		ai,
+		gesture
+	} = inputs;
+
+    gamesCount++;
+	output = `<p>Welcome <i>${name}</i>,</br> Time to see who won <i>${gamesCount}.</i> game!<p>`;
+	output += `<p>This match was against ${ai} opponent${ai == 1 ? '' : 's'}:</p><ul>`;
+
+	let game = new GameEngine();
+	for (let i = Number(ai); i--; i > 0) {
+		const aiName = `AI Player ${i+1}`;
+		output += `<li> <i>${aiName}</i> who played <i>${game.add(aiName)}</i></li>`;
+	}
+	output += `<li> And you played <i>${game.add(name, gesture)}</i></li></ul>`;
+
+	const result = game.execute();
+	if (result === 'Tie!') {
+        output += `<p>And the result is... <b>a Tie!</b><br/>
+                    Play one more time to find ultimate winner!</p>`
+	} else {
+		output += `<p>And the winner is... <i><b>${result}!</b></i><br/>`;
+		if (result !== name) {
+			output += `Try your luck one more time <i>${result}</i>!</p>`;
+		} else {
+			output += `Nice one <i>${result}</i>!</p>`;
+		}
+	}
+
+	return output;
+}
